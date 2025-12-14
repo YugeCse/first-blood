bb86f8d6cfdcb046451107002f645473f9a83ad3
Hwang <yugecosecant@foxmail.com>
2025-12-14 13:38:26 +0800

    #更新HUD的显示位置

--- scenes/scripts/main_scene.gd
+++ scenes/scripts/main_scene.gd
@@ -4,9 +4,18 @@ class_name MainScene extends Control
 @onready
 var player = $Camera2D/Player
 
+@onready
+var camera = $Camera2D
+
+@onready
+var hud_container = $HUDContainer
+
 func _ready() -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	hud_container.offset.x = absf(get_screen_position().x)
 
 ## 把子节点添加到相机中
 func add_child_to_camera(child: Node):
-	$Camera2D.add_child(child)
+	camera.add_child(child)
