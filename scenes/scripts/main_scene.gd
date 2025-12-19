06843f6db6808947698eee4b987f99bcfc68b07e
Hwang <yugecosecant@foxmail.com>
2025-12-19 22:18:47 +0800

    #更新细节

--- scenes/scripts/main_scene.gd
+++ scenes/scripts/main_scene.gd
@@ -3,11 +3,14 @@ class_name MainScene extends Control
 
 ## 玩家对象
 @onready
-var player = $Camera2D/Player
-	
+var player = $Player
+
+func _ready() -> void:
+	self.position.y = 0.0
+
 func _physics_process(_delta: float) -> void:
-	$Camera2D/HUDContainer.offset.x = absf(get_screen_position().x)
+	$HUDContainer.offset.x = absf(get_screen_position().x)
 
 ## 把子节点添加到相机中
-func add_child_to_camera(child: Node):
-	$Camera2D.add_child(child)
+func add_child_to_camera(child: Node) -> void:
+	add_child(child)
