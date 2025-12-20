c8e0e8239caa9b39589f52b95c01ad74777adb2f
Hwang <yugecosecant@foxmail.com>
2025-12-20 14:20:54 +0800

    #添加油桶爆炸飞行的逻辑

--- sprites/scripts/barrel.gd
+++ sprites/scripts/barrel.gd
@@ -33,11 +33,33 @@ var bom_collision_shape: CollisionShape2D
 ## 当前状态
 var state: Barrel.State = Barrel.State.idle
 
+## 是否把邻居激活了
+var _is_neigbor_active: bool = false
+
+## 已经飞行了的时间
+var _fly_time: float = 0.0
+
+## 飞行速度
+var _fly_speed: float = 100.0
+
+## 最大飞行距离
+var _max_fly_distance: float
+
+## 飞行方向
+var _fly_direction: Vector2
+
+## 开始飞行的坐标
+var _start_fly_position: Vector2
+
+## 是否到达最大距离了
+var _is_attach_max_distance: bool = false
+
 func _ready() -> void:
 	bom_sprite.animation_finished\
 		.connect(func(): state = State.destroy)
 	bom_area_detector.set_deferred(&'monitoring', false)
 	bom_area_detector.set_deferred(&'monitorable', false)
+	bom_sprite.frame_changed.connect(_on_bom_sprite_frame_changed)
 
 func _process(delta: float) -> void:
 	if state == State.idle:
@@ -46,7 +68,53 @@ func _process(delta: float) -> void:
 	elif state == State.bom:
 		sprite.visible = false
 		bom_sprite.visible = true
-	else: queue_free()
+	else: #已经被销毁
+		queue_free()
+		return
+	if _fly_direction: #如果已经在飞行了，执行抛物线
+		_fly_time += delta #累计计算飞行了的时间
+		var distance = global_position\
+			.distance_to(_start_fly_position)
+		if distance < _max_fly_distance:
+			if state == State.idle and _fly_time > 0.5:
+				state = State.bom
+			global_position += _fly_direction * _fly_speed * delta
+		else:
+			if not _is_attach_max_distance:
+				_is_attach_max_distance = true
+				_fly_direction = Vector2(1 if randi_range(0, 1) == 0 else -1, 0)
+			var velocity = _fly_direction * _fly_direction
+			velocity.y += 98.0 #重力加速度
+			global_position += velocity * delta #更新新的位置数据
+
+## 获取爆炸影响矩形数据
+func _get_bom_effect_area_rect() -> Rect2:
+	var shape = bom_collision_shape\
+		.shape as RectangleShape2D
+	return Rect2(global_position, shape.size)
+
+## 爆炸帧的变更事件
+func _on_bom_sprite_frame_changed():
+	var cur_rect = _get_bom_effect_area_rect()
+	if bom_sprite.frame_progress < 0.9:
+		if not _is_neigbor_active:
+			_is_neigbor_active = true
+			var barrel_nodes = get_tree()\
+				.get_nodes_in_group('Barrel')
+			var neigbors = barrel_nodes.filter(func(n): \
+				return n != self and\
+					(n as Barrel)._get_bom_effect_area_rect()\
+					.intersects(cur_rect))
+			if not neigbors or neigbors.size() == 0: return
+			for neigbor in neigbors:
+				neigbor._max_fly_distance = randf_range(35, 80)
+				neigbor._fly_speed = randf_range(100.0, 200.0)
+				neigbor._fly_direction = Vector2.from_angle(\
+					deg_to_rad(randf_range(190, 350)))
+				neigbor._start_fly_position = neigbor.global_position
+		return
+	bom_area_detector.set_deferred(&'monitoring', false)
+	bom_area_detector.set_deferred(&'monitorable', false)
 	
 ## 本地与其他物品发生碰撞，一般特指与子弹碰撞
 func _on_area_entered(area: Area2D) -> void:
@@ -59,7 +127,6 @@ func _on_area_entered(area: Area2D) -> void:
 		bom_area_detector.set_deferred(&'monitoring', true)
 		bom_area_detector.set_deferred(&'monitorable', true)
 		
-
 ## 爆炸物与其他发生碰撞，一般值得把敌人或者玩家炸死
 func _on_bom_area_2d_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
