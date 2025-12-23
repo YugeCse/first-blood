b9032a7f9d434a03f0cfd966f3843b7a26e1f515
Hwang <huangzhongyu@sph.com.sg>
2025-12-23 17:59:44 +0800

    #更新文件

--- scenes/scripts/main_scene.gd
+++ scenes/scripts/main_scene.gd
@@ -16,8 +16,11 @@ var viewport: SubViewport = $ViewportContainer/Viewport
 var viewport_container: SubViewportContainer = $ViewportContainer
 
 func _ready() -> void:
+	var support_web := OS.has_feature('web')
+	var support_touchscreen := DisplayServer.is_touchscreen_available()
 	_is_run_in_mobile =\
-		OS.get_name() in ['Android', 'iOS']
+		OS.get_name() in ['Android', 'iOS'] or\
+			(support_web and support_touchscreen)
 	_viewport_default_size = viewport.size #获取原有的默认大小
 	if not _is_run_in_mobile:
 		_adjust_viewport_size() #调整视窗尺寸信息
