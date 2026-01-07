6f476772cfb0f7cae82f8e95c50c3ae9ea51ab22
Hwang <huangzhongyu@sph.com.sg>
2026-01-07 17:56:27 +0800

    #增加一个例子系统效果

--- sprites/scripts/boss_laser.gd
+++ sprites/scripts/boss_laser.gd
@@ -1,49 +1,26 @@
 extends Node2D
 
 func _ready() -> void:
-	global_position = Vector2(150, 150)
-	$GPUParticles2D.texture = _create_atlas_texture(0)
-	$GPUParticles2D.process_material = _create_material()
-	$GPUParticles2D2.texture = _create_atlas_texture(1)
-	$GPUParticles2D2.process_material = _create_material()
-	$GPUParticles2D3.texture = _create_atlas_texture(2)
-	$GPUParticles2D3.process_material = _create_material()
-	$GPUParticles2D4.texture = _create_atlas_texture(3)
-	$GPUParticles2D4.process_material = _create_material()
-	$GPUParticles2D5.texture = _create_atlas_texture(4)
-	$GPUParticles2D5.process_material = _create_material()
-	$GPUParticles2D6.texture = _create_atlas_texture(5)
-	$GPUParticles2D6.process_material = _create_material()
-	#await get_tree().create_timer(0.2).timeout
-	#var tween = get_tree().create_tween()
-	#tween.set_loops()
-	#tween.tween_property(self, 'rotation_degrees', 360, 1.0)
-	#tween.tween_property(self, 'rotation_degrees', 0, 1.0)
-	#tween.play()
+	position = Vector2(150, 30)
+	var atlas_texture = AtlasTexture.new()
+	atlas_texture.atlas = preload('res://assets/boss/sParticlesLaser.png')
+	atlas_texture.region = Rect2(0, 0, 2, 2)
+	var gpu_particle = GPUParticles2D.new()
+	var g_material = ParticleProcessMaterial.new()
+	g_material.emission_shape =\
+		ParticleProcessMaterial.EMISSION_SHAPE_SPHERE
+	g_material.emission_sphere_radius = 20.0
+	g_material.initial_velocity_min = 10.0
+	g_material.initial_velocity_max = 100.0
+	g_material.direction = Vector3(0, -1, 0)
+	gpu_particle.emitting = true
+	gpu_particle.amount = 10
+	gpu_particle.lifetime = 0.5
+	gpu_particle.texture = atlas_texture
+	gpu_particle.position = Vector2.ZERO
+	gpu_particle.process_material = g_material
+	add_child(gpu_particle)
+	
 
 func _physics_process(delta: float) -> void:
-	global_position.x += delta * 100.0
-
-func _create_material() -> ParticleProcessMaterial:
-	@warning_ignore('shadowed_variable_base_class')
-	var material = ParticleProcessMaterial.new()
-	material.emission_shape = ParticleProcessMaterial.EMISSION_SHAPE_SPHERE_SURFACE
-	material.emission_sphere_radius = 10.0
-	material.emission_box_extents = Vector3(3,3,0)
-	material.scale_min = 0.2
-	material.scale_max = 0.5
-	material.angle_min = 0.0
-	material.angle_max = 25.0
-	material.gravity = Vector3(0, 0, 0)
-	material.initial_velocity_min = 0.0
-	material.initial_velocity_max = 5.0
-	material.direction = Vector3(-1, 0, 0)
-	material.spread = 25.0
-	return material
-
-func _create_atlas_texture(offset: int = 0) -> Texture2D:
-	var atlas_texture = AtlasTexture.new()
-	atlas_texture.atlas = preload('res://assets/boss/sparks.png')
-	atlas_texture.region = Rect2(Vector2(offset * 16, 0),\
-		Vector2(16, 16))
-	return atlas_texture
+	position.y += 80 * delta
